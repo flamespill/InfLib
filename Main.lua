8a661954c18ef5ac57787055fecc7b6cdcdd628f
flamespill <126897659+flamespill@users.noreply.github.com>
2025-02-16 21:19:29 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,7 @@
 --[[
 This is the Infinite Yield plugin, please read the tutorial on how to add a plugin to Infinite Yield here:
 https://github.com/flamespill/InfLib/blob/main/README.md#how-to-download
+
+Discord: discord.gg/nfkfKqUbGC
 ]]
 return loadstring(game:HttpGet("https://raw.githubusercontent.com/flamespill/InfLib/refs/heads/main/Core/Main.lua"))()
