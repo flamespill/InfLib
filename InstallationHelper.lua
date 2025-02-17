f9d7f63af11f0828491eae003a52bc72f0b25ab9
flamespill <126897659+flamespill@users.noreply.github.com>
2025-02-17 22:29:25 +0100

    Create InstallationHelper.lua

---
+++ InstallationHelper.lua
@@ -0,0 +1,6 @@
+--[[
+Once you have executed this, read the rest of the tutorial:
+https://github.com/flamespill/InfLib/blob/main/README.md#how-to-download
+]]
+if isfile("InfLib") then notify("InfLib", 'InfLib is already downloaded.') return end
+writefile("InfLib.iy", 'return loadstring(game:HttpGet("https://raw.githubusercontent.com/flamespill/InfLib/refs/heads/main/Core/Main.lua"))()')
