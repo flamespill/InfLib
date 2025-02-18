8de32cf57ab7c16376487465cf4937876f8c119d
flamespill <126897659+flamespill@users.noreply.github.com>
2025-02-18 18:00:44 +0100

    Delete InstallationHelper.lua

--- InstallationHelper.lua
+++
@@ -1,6 +0,0 @@
---[[
-NOTE: THIS IS NOT THE INFLIB PLUGIN, THIS IS A SCRIPT TO HELP YOU DOWNLOAD IT
-Once you have executed this, read the rest of the tutorial:
-https://github.com/flamespill/InfLib/blob/main/README.md#how-to-download
-]]
-writefile("InfLib.iy", 'return loadstring(game:HttpGet("https://raw.githubusercontent.com/flamespill/InfLib/refs/heads/main/Core/Main.lua"))()')
