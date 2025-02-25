6121d6776ab39c972a1c2c44036dc7cf8918bb6c
flamespill <126897659+flamespill@users.noreply.github.com>
2025-02-26 00:15:15 +0100

    Create Neckbreaker.lua

---
+++ Core/Plugins/Neckbreaker.lua
@@ -0,0 +1,31 @@
+local function PlayAnimation(Humanoid:Humanoid, AnimationId:number)
+	local Animation = Instance.new("Animation")
+	Animation.AnimationId = "rbxassetid://"..AnimationId
+	return(Humanoid:LoadAnimation(Animation))
+end
+
+local Plugin = {
+	["PluginCreator"] = "flamespill", -- Discord UserId: 1018142081878851595
+	["PluginName"] = "Neckbreaker",
+	["PluginDescription"] = "Adds a command that breaks your character's neck, that's it.",
+	["Commands"] = {
+
+		["breakneck"] = {
+			["ListName"] = "breakneck / necksnap / snapneck",
+			["Description"] = "Break your character's neck",
+			["Aliases"] = {"breakneck", "necksnap", "snapneck"},
+			["Function"] = function(args,speaker)
+				if r15(speaker) then notify("Neckbreaker", "Your rig must be R6.") return end
+				
+				local Animation = PlayAnimation(speaker.Character:FindFirstChildOfClass("Humanoid"), 35154961)
+				Animation:Play()
+				Animation:AdjustSpeed(3)
+				task.wait(.65) -- the reset being late is not a bug, ur ping is too high if that happens
+				execCmd("reset")
+			end
+		};
+
+	}
+}
+
+return Plugin
